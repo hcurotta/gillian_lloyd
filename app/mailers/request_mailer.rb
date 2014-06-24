735f1ace019c256f7db57db5e17138f0229de2b9
Harry Curotta <hcurotta@gmail.com>
2014-06-24 19:00:56 +1000

    fixed copy and update mail

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -2,6 +2,7 @@ class RequestMailer < ActionMailer::Base
   default from: "requests@no-reply.example.com"
 
   def callback_request(phone)
+    @phone = phone
     mail(:to => "hcurotta@gmail.com", :subject => "Website Phone Request: #{phone}")
   end
 end
